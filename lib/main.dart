a4ce0428db647451abaeefcd0658112bb5ed2a04
ewingapp <ewingappdev@gmail.com>
2023-05-10 19:41:55 +0700

    Updated terms and policy

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ Future<void> main() async {
       'appBlack': const Color.fromARGB(255, 54, 54, 54),
       'termsAndConditionsUrl': 'https://www.inito.dev/terms.html',
       'privacyPolicyUrl': 'https://www.inito.dev/privacy.html',
-      'contactUsUrl': 'https://www.inito.dev',
+      'contactUsUrl': 'https://initodev.freshdesk.com/support/tickets/new',
       //This appleAppStoreID is used for In-App Review.
       //Only change this ID with your Apple ID in App Store Connect under "General > App Information > Apple ID" if you publish this app into App Store.
       //Otherwise, leave it as it is.
